e63475e78531a1cf22f964f4ea0547cd7df7cb03
Roni Nevalainen <kitten@kittenz.dev>
2020-12-08 20:18:22 +0200

    Initial

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+	constructor(props: any) {
+		super(props);
+	}
+
+	render = () => (
+		<div id='page-home'>
+
+		</div>
+	);
+}
+
+export default HomePage;
