6041cc429ee0d17b6dd419df2314e1a5cf9f0ee1
Roni Nevalainen <31485942+Cinderkittenz@users.noreply.github.com>
2020-12-29 21:20:11 +0200

    Add new client code

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,8 +1,43 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-	entry: "./src/index.js",
+	entry: "./src/index.ts",
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx)$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader"
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: [ "*", ".tsx", ".ts", ".js" ]
+	},
 	output: {
-
-	}
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "./public"
+	},
+	devServer: {
+		port: 3000,
+		contentBase: path.join(__dirname, "public/"),
+		publicPath: "http://localhost:3000/dist/",
+		hotOnly: true
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
 }
