0cbf0ab36dc22af6a0fb6952b0492e6671e069ee
Roni Nevalainen <31485942+Cinderkittenz@users.noreply.github.com>
2021-01-04 16:04:11 +0200

    client: finish the layout of the top bar

--- packages/client/src/Components/TopBar/TopBar.tsx
+++ packages/client/src/Components/TopBar/TopBar.tsx
@@ -19,6 +19,18 @@ class TopBar extends React.Component {
 			{/* Navigation links */}
 			<div className={styles.NavigationContainer}>
 				<p className={styles.NavigationLink}>Home</p>
+				<div className={styles.Divider} />
+				<p className={styles.NavigationLink}>Portfolio</p>
+				<div className={styles.Divider} />
+				<p className={styles.NavigationLink}>Contact</p>
+			</div>
+
+			{/* Language & login buttons */}
+			<div className={styles.NavigationContainer}>
+				<p className={styles.NavigationLink}>Log in</p>
+				<div className={styles.Divider} />
+				<p className={styles.NavigationLink}>Language</p>
+				<div className={styles.Divider} />
 			</div>
 		</div>
 	);
