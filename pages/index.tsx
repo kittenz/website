69fc3c1de529e6a5dd60a7890583d7e9d90211e2
Roni Nevalainen <31485942+Cinderkittenz@users.noreply.github.com>
2021-01-15 22:38:58 +0200

    Replace JS file with TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Head from "next/head";
+
+import CopyrightNotice from "../src/components/CopyrightNotice/CopyrightNotice";
+
+import styles from "../styles/Home.module.css";
+
+class Home extends React.Component {
+	constructor(props: any) {
+		super(props);
+	}
+
+	render = () => (
+		<div id="page-content">
+			<Head>
+				<title>Kittenz • Home</title>
+			</Head>
+
+			<header>
+				<h1>Kittenz</h1>
+			</header>
+
+			<main>
+
+			</main>
+
+			<footer>
+				<CopyrightNotice holder="Kittenz" year={2018} />
+			</footer>
+		</div>
+	);
+}
+
+export default Home;
