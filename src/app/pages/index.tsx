a9c2bc82cc1d236d1ffd83300eb18ad2f95edd29
Roni Nevalainen <31485942+Cinderkittenz@users.noreply.github.com>
2021-01-16 22:04:37 +0200

    Create a navbar

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -16,9 +16,28 @@ class Home extends React.Component {
 				<title>Kittenz • Home</title>
 			</Head>
 
-			<header>
-				<h1>Kittenz</h1>
-			</header>
+			{ /* Navbar */}
+			<nav className="navbar">
+				<ul className="navbar-nav">
+					{/* Start */}
+					<li className="nav-item">
+						<img src="images/logo.png" width="64" height="64" className="rounded"></img>
+					</li>
+
+					{/* Center */}
+					<li className="nav-item">
+						<ul className="navbar-nav">
+							<li className="nav-item"><p>About</p></li>
+							<li className="nav-item"><p>Contact</p></li>
+						</ul>
+					</li>
+
+					{/* End */}
+					<li className="nav-item">
+					<p>sdaadasd</p>
+					</li>
+				</ul>
+			</nav>
 
 			<main>
 
