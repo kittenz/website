5f61dd69d796b9c8fb747224b4a107b664458062
Roni Nevalainen <31485942+Cinderkittenz@users.noreply.github.com>
2021-01-28 18:49:02 +0200

    Add project pages

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -24,7 +24,8 @@ let Home = () => {
 					{/* Center */}
 					<li className="nav-item">
 						<ul className="navbar-nav">
-							<li className="nav-item"><p>About</p></li>
+							<li className="nav-item"><p>Projects</p></li>
+							<li className="nav-item"><p>Portfolio</p></li>
 							<li className="nav-item"><p>Contact</p></li>
 						</ul>
 					</li>
@@ -40,7 +41,7 @@ let Home = () => {
 			</nav>
 
 			<main>
-
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</main>
 
 			<footer>
