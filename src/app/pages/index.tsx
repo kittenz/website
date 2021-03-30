d6653937f182fa43001f93abcb5858c253c48478
Roni Nevalainen <31485942+Cinderkittenz@users.noreply.github.com>
2021-03-30 12:31:45 +0300

    website updates & package upgrade to fix a vulnerability

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { createRef, useRef } from "react";
 import Head from "next/head";
 
 import styles from "../styles/Home.module.css";
 
-const Home = () => {
-	return (
+class Home extends React.Component {
+	logoContainerRef: React.RefObject<HTMLDivElement>;
+	textRef: React.RefObject<HTMLDivElement>;
+
+	constructor(props: any) {
+		super(props);
+
+		this.logoContainerRef = createRef();
+		this.textRef = createRef();
+	}
+
+	componentDidMount = () => {
+		if (this.logoContainerRef && this.logoContainerRef.current) {
+			this.logoContainerRef.current.className += ` ${styles.animateLogoContainer}`;
+		}
+
+		if (this.textRef && this.textRef.current) {
+			this.textRef.current.className += ` ${styles.animateText}`;
+		}
+	}
+
+	render = () => (
 		<div className={styles.page}>
 			<Head>
 				<title>Kittenz • Home</title>
 			</Head>
 
 			<main className={styles.main}>
-				<div className={styles.logoContainer}>
+				<div ref={this.logoContainerRef} className={styles.logoContainer}>
 					<img src="images/logo.png" className={`rounded ${styles.logo}`} />
 					<h1 className={styles.logoText}>Kittenz</h1>
 				</div>
 
-				<div className={`text-primary ${styles.text}`} style={{textAlign: "center"}}>
+				<div ref={this.textRef} className={`text-primary ${styles.text}`} style={{textAlign: "center"}}>
 					<p>Hi! I'm Kittenz.</p>
 					<br />
 					<p>My website is coming soon.</p>
@@ -25,5 +45,22 @@ const Home = () => {
 		</div>
 	);
 }
+
+/*
+const Home = () => {
+	const logoContainerRef = useRef(null);
+	const textRef = useRef(null);
+
+	document.onreadystatechange = () => {
+		if (document.readyState === "complete") {
+			
+		}
+	}
+
+	return (
+		
+	);
+}
+*/
 
 export default Home;
